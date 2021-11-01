238ced2c5d3b320236ced015804d53f3af050198
Nick Nguyen <55971813+ninhdangcap123@users.noreply.github.com>
2021-11-01 16:26:03 +0700

    Update front end

--- COMP1640_Coursework/resources/views/category/index.blade.php
+++ COMP1640_Coursework/resources/views/category/index.blade.php
@@ -1,27 +1,63 @@
 @extends('layouts.header')
 @section('content')
-<a href="{{route('qam.create')}}">Create new Category</a> <hr>
-<table border="1">
-    <tr>
-        <td>ID</td>
-        <td>Name</td>
-    </tr>
-    @foreach($categories as $category)
+{{--<a href="{{route('qam.create')}}">Create new Category</a> <hr>--}}
+{{--<table border="1">--}}
+{{--    <tr>--}}
+{{--        <td>ID</td>--}}
+{{--        <td>Name</td>--}}
+{{--    </tr>--}}
+{{--    @foreach($categories as $category)--}}
 
+{{--        <tr>--}}
+{{--            <td>{{ $category->id }}</td>--}}
+{{--            <td>{{ $category->name }}</td>--}}
+{{--            <td>--}}
+{{--                <form action="{{ route('qam.delete', $category->id) }}" method="POST">--}}
+{{--                    @csrf--}}
+{{--                    @method('delete')--}}
+{{--                    <button type="submit" class="btn btn-outline-danger">Delete</button>--}}
+{{--                </form>--}}
+{{--            </td>--}}
+
+
+{{--        </tr>--}}
+
+{{--@endforeach--}}
+{{--</table>--}}
+<div class="container">
+    <div class="row" style="margin-bottom: 20px;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h3>Category</h3>
+            </div>
+            <div class="pull-right" style="margin-top: 20px;">
+                <a class="btn btn-success" href="{{ route('qam.create') }}">Add Category
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <table class="table table-bordered">
         <tr>
-            <td>{{ $category->id }}</td>
-            <td>{{ $category->name }}</td>
-            <td>
-                <form action="{{ route('qam.delete', $category->id) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-outline-danger">Delete</button>
-                </form>
-            </td>
-
-
+            <th>ID</th>
+            <th>Name</th>
+            <th width="280px">Action</th>
         </tr>
+        @foreach ($categories as $category)
+            <tr>
+                <td>{{ $category->id }}</td>
+                <td>{{ $category->name }}</td>
+                <td>
+                    <form action="{{ route('qam.delete', $category->id) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-outline-danger">Delete</button>
+                    </form>
+                </td>
 
-@endforeach
-</table>
+            </tr>
+        @endforeach
+    </table>
+</div>
 @endsection
